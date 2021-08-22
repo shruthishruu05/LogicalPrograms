38774c9b47781ee0e6537b5098443694d20d600b
shruthishruu05 <shruthi.m@perfios.com>
2021-08-22 17:24:03 +0530

    MODIFIED: Distance Program

--- src/com/bridgelabz/FunctionalPrograms/Distance.java
+++ src/com/bridgelabz/FunctionalPrograms/Distance.java
@@ -4,11 +4,7 @@ public class Distance {
 	public static void main(String[] args) {
 		int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
-
-        
-        double distance = Math.sqrt(x*x + y*y);
- 
-        
+        double distance = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
         System.out.println("distance from (" + x + ", " + y + ") to (0, 0) = " + distance);
     }   
 }
